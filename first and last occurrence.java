e567234255db7069dae9af37d5d2290f9569d945
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-07-10 19:00:41 +0530

    Create first and last occurrence.java

---
+++ first and last occurrence.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FirstAndLastOccurence {
+    public static int first = -1;
+    public static int last = -1;
+    public static void count(String x, int idx, char element){
+        if (idx == x.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currchar = x.charAt(idx);
+        if (currchar == element){
+            if (first == -1){
+                first = idx;
+            }else last = idx;
+        }
+        count(x, idx+1, element);
+    }
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        int idx = 0;
+        count(x, idx, 'e');
+    }
+}
