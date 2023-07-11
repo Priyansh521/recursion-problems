d34e5f9ebafc2f11a29fa90ea3e23c3997955ee9
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-07-11 21:42:13 +0530

    Create moveElement.java

---
+++ moveElement.java
@@ -0,0 +1,27 @@
+//we have to move a specified multi occurring element to the end
+public class MoveAnElement {
+    public static void move(String x, int count, int idx,  String newStr){
+        if (idx == x.length()-1){
+          // for loop to add x at the end of the new str
+            for (int i = 0; i< count; i++){
+                newStr += 'x';
+            }
+            System.out.println(newStr);
+            return;
+        }
+        if (x.charAt(idx) == 'x'){
+            count++ ;// count is to keep track of how  many x are there in the given string
+            move(x, count, idx+1, newStr);
+        }else {
+            newStr += x.charAt(idx);
+            move(x, count, idx+1, newStr);
+        }
+    }
+    public static void main(String[] args) {
+        String x = "aaxxxbhbxxxx";
+        int count = 0;
+        int idx = 0;
+        String newStr = "";
+        move( x, count, idx, newStr);
+    }
+}
